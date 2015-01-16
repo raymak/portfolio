5992ae15d226f7ef27bf790163c4713f5e97839c
raymak <kamyar.ardekani@gmail.com>
2015-01-16 14:33:14 -0800

    more projects added, a few style modifications

---
+++ generators/projlist.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'json'
+
+if !(ARGV[0] && ARGV[1])
+	puts "missing file names"
+	puts "Usage: \nprojlist.rb projectsfile templatefile [output]"
+	exit
+end
+
+def replaceString(project, template)
+	project.inject(template) do |out, (k, v)|
+		out.gsub("%" + k + "%", v)
+	end
+end
+
+
+projectsFile = ARGV[0]
+templateFile = ARGV[1]
+puts ARGV[2]
+outFile = ARGV[2] || "projlist.html.partial"
+
+projects = JSON.parse(File.read(projectsFile))
+
+template = File.read(templateFile)
+
+output += projects.inject("") do |out, project|
+	out + replaceString(project, template)
+end
+
+
+puts outFile
+
+File.write(outFile, output)
+
